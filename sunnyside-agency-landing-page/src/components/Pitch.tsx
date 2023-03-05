91c47647a3d30ae41a9c40942fd8f76fe54a06fe
mannewdan <109920968+mannewdan@users.noreply.github.com>
2023-03-04 18:59:03 -0600

    responsive pitch section

--- sunnyside-agency-landing-page/src/components/Pitch.tsx
+++ sunnyside-agency-landing-page/src/components/Pitch.tsx
@@ -2,27 +2,31 @@ export default function Pitch() {
   return (
     <section className="pitch">
       <div className="item-split">
-        <h2>Transform your brand</h2>
-        <p>
-          {
-            "We are a full-service creative agency specializing in helping brands grow fast. Engage your clients through compelling visuals that do most of the marketing for you."
-          }
-        </p>
-        <a href="#" className="yellow">
-          Learn More
-        </a>
+        <div className="container">
+          <h2>Transform your brand</h2>
+          <p>
+            {
+              "We are a full-service creative agency specializing in helping brands grow fast. Engage your clients through compelling visuals that do most of the marketing for you."
+            }
+          </p>
+          <a href="#" className="yellow">
+            Learn More
+          </a>
+        </div>
         <img src="images/image-transform.jpg" alt="an egg"></img>
       </div>
       <div className="item-split">
-        <h2>Stand out to the right audience</h2>
-        <p>
-          {
-            "Using a collaborative formula of designers, researchers, photographers, videographers, and copywriters, we’ll build and extend your brand in digital places. "
-          }
-        </p>
-        <a href="#" className="pink">
-          Learn More
-        </a>
+        <div className="container">
+          <h2>Stand out to the right audience</h2>
+          <p>
+            {
+              "Using a collaborative formula of designers, researchers, photographers, videographers, and copywriters, we’ll build and extend your brand in digital places. "
+            }
+          </p>
+          <a href="#" className="pink">
+            Learn More
+          </a>
+        </div>
         <img src="images/image-stand-out.jpg" alt="a glass"></img>
       </div>
       <div className="item graphic-design">
