cb5537b770b2e154a98ba240e099196a16d7e141
mannewdan <109920968+mannewdan@users.noreply.github.com>
2023-03-05 02:28:29 -0600

    testimonials

--- sunnyside-agency-landing-page/src/components/Testimonials.tsx
+++ sunnyside-agency-landing-page/src/components/Testimonials.tsx
@@ -1,3 +1,39 @@
 export default function Testimonials() {
-  return <section>testimonials</section>;
+  return (
+    <section>
+      <h3 className="testimonials-heading">Client Testimonials</h3>
+      <div className="testimonials">
+        <blockquote className="item">
+          <strong className="name">Emily R.</strong>
+          <p className="title">Marketing Director</p>
+          <p className="quote">
+            {
+              "We put our trust in Sunnyside and they delivered, making sure our needs were met and deadlines were always hit."
+            }
+          </p>
+          <img src="images/image-emily.jpg"></img>
+        </blockquote>{" "}
+        <blockquote className="item">
+          <strong className="name">Thomas S.</strong>
+          <p className="title">Chief Operating Officer</p>
+          <p className="quote">
+            {
+              "Sunnyside’s enthusiasm coupled with their keen interest in our brand’s success made it a satisfying and enjoyable experience."
+            }
+          </p>
+          <img src="images/image-thomas.jpg"></img>
+        </blockquote>{" "}
+        <blockquote className="item">
+          <strong className="name">Jennie F.</strong>
+          <p className="title">Business Owner</p>
+          <p className="quote">
+            {
+              "Incredible end result! Our sales increased over 400% when we worked with Sunnyside. Highly recommended!"
+            }
+          </p>
+          <img src="images/image-jennie.jpg"></img>
+        </blockquote>
+      </div>
+    </section>
+  );
 }
