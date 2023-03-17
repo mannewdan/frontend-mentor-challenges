79545182d5dc5de5f823902580462f56933bd09d
mannewdan <109920968+mannewdan@users.noreply.github.com>
2023-03-16 19:36:32 -0500

    larger screen styling

--- multi-step-form/src/components/FormStepPlan.tsx
+++ multi-step-form/src/components/FormStepPlan.tsx
@@ -10,56 +10,60 @@ export default function FormStepPlan({ formData, setFormData }: FormStepProps) {
         <p>You have the option of monthly or yearly billing.</p>
       </header>
 
-      <button
-        className={formData.planType === PlanInfo.arcade.id ? "selected" : ""}
-        onClick={(e) => {
-          e.preventDefault();
-          setFormData((prev) => {
-            return { ...prev, planType: PlanInfo.arcade.id };
-          });
-        }}
-      >
-        <img src="images/icon-arcade.svg"></img>
-        <p className="name">Arcade</p>
-        <p className="price">{`$${
-          PlanInfo.arcade.price * (isYearly ? 10 : 1)
-        }/${isYearly ? "yr" : "mo"}`}</p>
-        {isYearly && <p className="savings">2 months free</p>}
-      </button>
+      <div className="plans">
+        <button
+          className={formData.planType === PlanInfo.arcade.id ? "selected" : ""}
+          onClick={(e) => {
+            e.preventDefault();
+            setFormData((prev) => {
+              return { ...prev, planType: PlanInfo.arcade.id };
+            });
+          }}
+        >
+          <img src="images/icon-arcade.svg"></img>
+          <p className="name">Arcade</p>
+          <p className="price">{`$${
+            PlanInfo.arcade.price * (isYearly ? 10 : 1)
+          }/${isYearly ? "yr" : "mo"}`}</p>
+          {isYearly && <p className="savings">2 months free</p>}
+        </button>
 
-      <button
-        className={formData.planType === PlanInfo.advanced.id ? "selected" : ""}
-        onClick={(e) => {
-          e.preventDefault();
-          setFormData((prev) => {
-            return { ...prev, planType: PlanInfo.advanced.id };
-          });
-        }}
-      >
-        <img src="images/icon-advanced.svg"></img>
-        <p className="name">Advanced</p>
-        <p className="price">{`$${
-          PlanInfo.advanced.price * (isYearly ? 10 : 1)
-        }/${isYearly ? "yr" : "mo"}`}</p>
-        {isYearly && <p className="savings">2 months free</p>}
-      </button>
+        <button
+          className={
+            formData.planType === PlanInfo.advanced.id ? "selected" : ""
+          }
+          onClick={(e) => {
+            e.preventDefault();
+            setFormData((prev) => {
+              return { ...prev, planType: PlanInfo.advanced.id };
+            });
+          }}
+        >
+          <img src="images/icon-advanced.svg"></img>
+          <p className="name">Advanced</p>
+          <p className="price">{`$${
+            PlanInfo.advanced.price * (isYearly ? 10 : 1)
+          }/${isYearly ? "yr" : "mo"}`}</p>
+          {isYearly && <p className="savings">2 months free</p>}
+        </button>
 
-      <button
-        className={formData.planType === PlanInfo.pro.id ? "selected" : ""}
-        onClick={(e) => {
-          e.preventDefault();
-          setFormData((prev) => {
-            return { ...prev, planType: PlanInfo.pro.id };
-          });
-        }}
-      >
-        <img src="images/icon-pro.svg"></img>
-        <p className="name">Pro</p>
-        <p className="price">{`$${PlanInfo.pro.price * (isYearly ? 10 : 1)}/${
-          isYearly ? "yr" : "mo"
-        }`}</p>
-        {isYearly && <p className="savings">2 months free</p>}
-      </button>
+        <button
+          className={formData.planType === PlanInfo.pro.id ? "selected" : ""}
+          onClick={(e) => {
+            e.preventDefault();
+            setFormData((prev) => {
+              return { ...prev, planType: PlanInfo.pro.id };
+            });
+          }}
+        >
+          <img src="images/icon-pro.svg"></img>
+          <p className="name">Pro</p>
+          <p className="price">{`$${PlanInfo.pro.price * (isYearly ? 10 : 1)}/${
+            isYearly ? "yr" : "mo"
+          }`}</p>
+          {isYearly && <p className="savings">2 months free</p>}
+        </button>
+      </div>
 
       <div className="interval-toggle">
         <span className={!isYearly ? "active" : ""}>Monthly</span>
