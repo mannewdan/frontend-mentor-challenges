c8265d8af737e2d5cb590a319f6a67fa6cb780c6
mannewdan <109920968+mannewdan@users.noreply.github.com>
2023-03-23 02:29:11 -0500

    horizontal scroll

--- entertainment-web-app/src/components/HorizontalScroll.tsx
+++ entertainment-web-app/src/components/HorizontalScroll.tsx
@@ -1,17 +1,52 @@
+import React from "react";
+
 type HorizontalScrollProps = {
   children?: React.ReactNode;
 };
 
 export default function HorizontalScroll({ children }: HorizontalScrollProps) {
+  const [xOffset, setXOffset] = React.useState(0);
+  const ref = React.useRef<HTMLDivElement>(null);
+
+  const scrollHorizontally = (e: WheelEvent) => {
+    //disable vertical scroll
+    e.preventDefault();
+
+    //grab scroll amount & preserve x scroll behavior
+    const delta = e.deltaY ? e.deltaY : e.deltaX;
+
+    //calculate boundaries
+    const containerWidth = ref.current?.clientWidth;
+    const scrollerWidth = ref.current?.firstElementChild?.scrollWidth;
+    const max = 0;
+    const min =
+      !containerWidth || !scrollerWidth
+        ? max
+        : Math.min(max, containerWidth - scrollerWidth);
+
+    setXOffset((prev) => {
+      return Math.max(min, Math.min(max, prev - delta));
+    });
+  };
+
+  React.useEffect(() => {
+    //manually add event listener this way to make it not passive
+    ref.current?.addEventListener("wheel", scrollHorizontally, {
+      passive: false,
+    });
+    return () => {
+      ref.current?.removeEventListener("wheel", scrollHorizontally);
+    };
+  }, []);
+
   return (
-    <div
-      onMouseEnter={() => {}}
-      onWheel={(e) => {
-        e.preventDefault();
-      }}
-      className="x-scroll"
-    >
-      {children}
+    <div ref={ref} className="x-scroll-container">
+      <div
+        className="x-scroller"
+        style={{ transform: `translateX(${xOffset}px)` }}
+      >
+        {children}
+      </div>
     </div>
   );
 }
