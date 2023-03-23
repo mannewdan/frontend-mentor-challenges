bc2ab04e6a7cbb94b78f4e9b2bd7eaf577d03c64
mannewdan <109920968+mannewdan@users.noreply.github.com>
2023-03-23 17:33:06 -0500

    horizontal scroll polishing

--- entertainment-web-app/src/components/HorizontalScroll.tsx
+++ entertainment-web-app/src/components/HorizontalScroll.tsx
@@ -1,44 +1,63 @@
 import React from "react";
+import useWindowResized from "../hooks/useWindowResized";
 
 type HorizontalScrollProps = {
   children?: React.ReactNode;
 };
 
 export default function HorizontalScroll({ children }: HorizontalScrollProps) {
-  const [xOffset, setXOffset] = React.useState(0);
+  const [xOffset, setXOffset] = React.useState(24);
+  const { windowWidth } = useWindowResized();
   const ref = React.useRef<HTMLDivElement>(null);
 
-  const scrollHorizontally = (e: WheelEvent) => {
+  //functions
+  const handleWheelEvent = (e: WheelEvent) => {
     //disable vertical scroll
     e.preventDefault();
 
     //grab scroll amount & preserve x scroll behavior
     const delta = e.deltaY ? e.deltaY : e.deltaX;
 
+    scrollHorizontally(delta);
+  };
+  const scrollHorizontally = (delta: number) => {
     //calculate boundaries
+    const margin = parseFloat(
+      getComputedStyle(ref.current as Element).fontSize
+    );
     const containerWidth = ref.current?.clientWidth;
     const scrollerWidth = ref.current?.firstElementChild?.scrollWidth;
-    const max = 0;
+    const max = margin;
     const min =
       !containerWidth || !scrollerWidth
         ? max
-        : Math.min(max, containerWidth - scrollerWidth);
+        : Math.min(max, containerWidth - scrollerWidth - margin);
 
     setXOffset((prev) => {
       return Math.max(min, Math.min(max, prev - delta));
     });
   };
 
+  //effects
   React.useEffect(() => {
+    const margin = parseFloat(
+      getComputedStyle(ref.current as Element).fontSize
+    );
+    setXOffset(margin);
+
     //manually add event listener this way to make it not passive
-    ref.current?.addEventListener("wheel", scrollHorizontally, {
+    ref.current?.addEventListener("wheel", handleWheelEvent, {
       passive: false,
     });
     return () => {
-      ref.current?.removeEventListener("wheel", scrollHorizontally);
+      ref.current?.removeEventListener("wheel", handleWheelEvent);
     };
   }, []);
+  React.useEffect(() => {
+    scrollHorizontally(0);
+  }, [windowWidth]);
 
+  //rendering
   return (
     <div ref={ref} className="x-scroll-container">
       <div
---
+++ entertainment-web-app/src/hooks/useWindowResized.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default function useWindowResize() {
+  const [windowWidth, setWindowWidth] = React.useState(window.innerWidth);
+
+  const handleResize = () => {
+    setWindowWidth(window.innerWidth);
+  };
+  React.useEffect(() => {
+    window.addEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  return { windowWidth };
+}
