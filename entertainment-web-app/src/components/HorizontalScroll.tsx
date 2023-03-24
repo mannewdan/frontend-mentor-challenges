6c46e47d7e5a3935d43f431fde667757bdb93459
mannewdan <109920968+mannewdan@users.noreply.github.com>
2023-03-24 16:01:39 -0500

    horizontal scroll cleanup & polish

--- entertainment-web-app/src/components/HorizontalScroll.tsx
+++ entertainment-web-app/src/components/HorizontalScroll.tsx
@@ -6,15 +6,14 @@ type HorizontalScrollProps = {
 };
 
 export default function HorizontalScroll({ children }: HorizontalScrollProps) {
-  const [xOffset, setXOffset] = React.useState(24);
+  const [posParams, setPosParams] = React.useState({ xOffset: 24, margin: 24 });
   const { windowWidth } = useWindowResized();
   const ref = React.useRef<HTMLDivElement>(null);
 
   //functions
   const handleWheelEvent = (e: WheelEvent) => {
     if (checkScrollable()) {
-      //disable vertical scroll
-      e.preventDefault();
+      e.preventDefault(); //disable vertical scroll
     } else {
       return;
     }
@@ -25,36 +24,41 @@ export default function HorizontalScroll({ children }: HorizontalScrollProps) {
     scrollHorizontally(delta);
   };
   const scrollHorizontally = (delta: number) => {
+    setPosParams((prev) => {
+      return {
+        ...prev,
+        xOffset: clampOffset(prev.xOffset - delta, prev.margin),
+      };
+    });
+  };
+  const checkScrollable = (): boolean => {
+    if (!ref.current || !ref.current.firstElementChild) return false;
+    const containerWidth = ref.current.clientWidth;
+    const scrollerWidth = ref.current.firstElementChild.scrollWidth;
+    return scrollerWidth >= containerWidth;
+  };
+  const clampOffset = (offset: number, margin: number): number => {
+    if (!ref.current || !ref.current.firstElementChild) return offset;
+
     //calculate boundaries
-    const margin = parseFloat(
-      getComputedStyle(ref.current as Element).fontSize
-    );
-    const containerWidth = ref.current?.clientWidth;
-    const scrollerWidth = ref.current?.firstElementChild?.scrollWidth;
+    const containerWidth = ref.current.clientWidth;
+    const scrollerWidth = ref.current.firstElementChild.scrollWidth;
     const max = margin;
     const min =
       !containerWidth || !scrollerWidth
         ? max
         : Math.min(max, containerWidth - scrollerWidth - margin);
 
-    setXOffset((prev) => {
-      return Math.max(min, Math.min(max, prev - delta));
-    });
-  };
-  const checkScrollable = (): boolean => {
-    const containerWidth = ref.current?.clientWidth;
-    const scrollerWidth = ref.current?.firstElementChild?.scrollWidth;
-
-    if (!containerWidth || !scrollerWidth) return false;
-    return scrollerWidth >= containerWidth;
+    return Math.max(min, Math.min(max, offset));
   };
 
   //effects
   React.useEffect(() => {
+    //set initial margin & offset
     const margin = parseFloat(
       getComputedStyle(ref.current as Element).fontSize
     );
-    setXOffset(margin);
+    setPosParams({ xOffset: margin, margin });
 
     //manually add event listener this way to make it not passive
     ref.current?.addEventListener("wheel", handleWheelEvent, {
@@ -65,7 +69,20 @@ export default function HorizontalScroll({ children }: HorizontalScrollProps) {
     };
   }, []);
   React.useEffect(() => {
-    scrollHorizontally(0);
+    //on resize, recalculate margin
+    const margin = parseFloat(
+      getComputedStyle(ref.current as Element).fontSize
+    );
+    setPosParams((prev) => {
+      //snap xOffset to the new margin if it's close
+      const xOffset =
+        prev.margin !== margin && Math.abs(prev.xOffset - margin) < 50
+          ? margin
+          : prev.xOffset;
+
+      //on resize, clamp xOffset so that the content always fits on the page
+      return { xOffset: clampOffset(xOffset, margin), margin };
+    });
   }, [windowWidth]);
 
   //rendering
@@ -73,7 +90,7 @@ export default function HorizontalScroll({ children }: HorizontalScrollProps) {
     <div ref={ref} className="x-scroll-container">
       <div
         className="x-scroller"
-        style={{ transform: `translateX(${xOffset}px)` }}
+        style={{ transform: `translateX(${posParams.xOffset}px)` }}
       >
         {children}
       </div>
