fa33f964c85eb950e6a54765ffb6b63c0ff756a0
mannewdan <109920968+mannewdan@users.noreply.github.com>
2023-03-25 18:41:48 -0500

    video text formatting

--- entertainment-web-app/src/components/Video.tsx
+++ entertainment-web-app/src/components/Video.tsx
@@ -9,6 +9,7 @@ type VideoProps = {
 export default function Video({ video, trending, imgSize }: VideoProps) {
   const imgEl = (
     <img
+      className="thumbnail"
       src={(() => {
         if (trending && video.thumbnail.trending) {
           return video.thumbnail.trending[
@@ -25,7 +26,27 @@ export default function Video({ video, trending, imgSize }: VideoProps) {
     <article className={`video ${trending ? "trending" : ""}`}>
       {imgEl}
 
-      <p>{"" + trending}</p>
+      <div className={`text-container ${trending ? "trending" : ""}`}>
+        <h3 className={`title ${trending ? "text-h-s" : "text-h-xs"}`}>
+          {video.title}
+        </h3>
+
+        <div
+          className={`info-group text-white-75 ${
+            trending ? "text-b-m" : "text-b-s"
+          }`}
+        >
+          <p className="year">{video.year}</p>
+          <div className="category">
+            <img
+              className="category-icon"
+              src="assets/icon-category-movie.svg"
+            ></img>
+            <p>{video.category}</p>
+          </div>
+          <p className="rating">{video.rating}</p>
+        </div>
+      </div>
     </article>
   );
 }
