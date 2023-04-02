1af4d3840f95a1b1eff6b304a61992bf62a0256d
mannewdan <109920968+mannewdan@users.noreply.github.com>
2023-04-02 02:08:16 -0500

    kanban task manager initial

---
+++ kanban-task-management-app/src/App.tsx
@@ -0,0 +1,9 @@
+import Design from "./components/Design";
+
+export default function App() {
+  return (
+    <>
+      <Design />
+    </>
+  );
+}
---
+++ kanban-task-management-app/src/components/Design.tsx
@@ -0,0 +1,13 @@
+export default function Design() {
+  //  document.body.classList.add("dark");
+
+  return (
+    <div>
+      <h1>design</h1>
+      <h2>design</h2>
+      <h3>design</h3>
+      <p>design</p>
+      <p>design</p>
+    </div>
+  );
+}
