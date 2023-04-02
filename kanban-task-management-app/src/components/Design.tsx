b58bf17b3e6496060b85dd8f7e508c3a45e620f1
mannewdan <109920968+mannewdan@users.noreply.github.com>
2023-04-02 13:37:08 -0500

    typography

--- kanban-task-management-app/src/components/Design.tsx
+++ kanban-task-management-app/src/components/Design.tsx
@@ -2,12 +2,31 @@ export default function Design() {
   //  document.body.classList.add("dark");
 
   return (
-    <div>
-      <h1>design</h1>
-      <h2>design</h2>
-      <h3>design</h3>
-      <p>design</p>
-      <p>design</p>
-    </div>
+    <section className="flow">
+      <h1 className="text-h-xl">Heading XL</h1>
+      <h2 className="text-h-l">Heading L</h2>
+      <h3 className="text-h-m">Heading M</h3>
+      <h4 className="text-h-s">Heading S</h4>
+      <p className="text-b-l c-text-main-25">
+        Body L - Lorem ipsum dolor sit amet, consectetuer adipiscing elit.
+        Phasellus hendrerit. Pellentesque aliquet nibh nec urna. In nisi neque,
+        aliquet vel, dapibus id, mattis vel, nisi. Sed pretium, ligula
+        sollicitudin laoreet viverra, tortor libero sodales leo, eget blandit
+        nunc tortor eu nibh. Nullam mollis. Ut justo. Suspendisse potenti. Sed
+        egestas, ante et vulputate volutpat, eros pede semper est, vitae luctus
+        metus libero eu augue. Morbi purus libero, faucibus adipiscing, commodo
+        quis, gravida id, est.
+      </p>
+      <p className="text-b-m c-text-neutral">
+        Body M - Lorem ipsum dolor sit amet, consectetuer adipiscing elit.
+        Phasellus hendrerit. Pellentesque aliquet nibh nec urna. In nisi neque,
+        aliquet vel, dapibus id, mattis vel, nisi. Sed pretium, ligula
+        sollicitudin laoreet viverra, tortor libero sodales leo, eget blandit
+        nunc tortor eu nibh. Nullam mollis. Ut justo. Suspendisse potenti. Sed
+        egestas, ante et vulputate volutpat, eros pede semper est, vitae luctus
+        metus libero eu augue. Morbi purus libero, faucibus adipiscing, commodo
+        quis, gravida id, est.
+      </p>
+    </section>
   );
 }
