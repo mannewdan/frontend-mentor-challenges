17564cff1055b7fb81fe805bf72946a2c0beba34
mannewdan <109920968+mannewdan@users.noreply.github.com>
2023-04-08 23:55:10 -0500

    mobile sidebar

--- kanban-task-management-app/src/App.tsx
+++ kanban-task-management-app/src/App.tsx
@@ -1,4 +1,3 @@
-import Design from "./components/Design";
 import Header from "./components/Header";
 import Sidebar from "./components/Sidebar";
 
--- kanban-task-management-app/src/components/Sidebar.tsx
+++ kanban-task-management-app/src/components/Sidebar.tsx
@@ -1,7 +1,46 @@
+import boardIcon from "../assets/icon-board.svg";
+import hideIcon from "../assets/icon-hide-sidebar.svg";
+import showIcon from "../assets/icon-show-sidebar.svg";
+import lightIcon from "../assets/icon-light-theme.svg";
+import darkIcon from "../assets/icon-dark-theme.svg";
+import Icon from "./Icon";
+
 export default function Sidebar() {
+  const boards = ["Platform Launch", "Marketing Plan", "Roadmap"];
+  const boardEls = boards.map((item, index) => {
+    return (
+      <button className={`button-nav ${index === 0 ? "active" : ""}`}>
+        <Icon url={boardIcon} />
+        {item}
+      </button>
+    );
+  });
+
   return (
-    <nav>
-      <div>sidebar</div>
+    <nav className="sidebar">
+      <div className="scrollable-area">
+        <h2 className="text-h-s">All Boards (3)</h2>
+
+        <div className="container">
+          {boardEls}
+          <button className="button-nav add">
+            <Icon url={boardIcon} />+ Create New Board
+          </button>
+        </div>
+      </div>
+
+      <div className="dark-toggle">
+        <img className="light-icon" src={lightIcon}></img>
+        <button className="dark"></button>
+        <img className="dark-icon" src={darkIcon}></img>
+      </div>
+
+      <div className="hide-button container">
+        <button className="button-nav">
+          <Icon url={hideIcon} />
+          Hide Sidebar
+        </button>
+      </div>
     </nav>
   );
 }
