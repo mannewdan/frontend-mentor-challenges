71854f161d4e631b69f375e94a1f1f7a586298e2
mannewdan <109920968+mannewdan@users.noreply.github.com>
2023-04-11 02:49:15 -0500

    no transitions on load

--- kanban-task-management-app/src/App.tsx
+++ kanban-task-management-app/src/App.tsx
@@ -1,8 +1,16 @@
+import React from "react";
+
 import Header from "./components/Header";
 import Sidebar from "./components/Sidebar";
 import DataContext from "./context/DataContext";
 
 export default function App() {
+  React.useEffect(() => {
+    setTimeout(() => {
+      document.body.classList.remove("notransition");
+    }, 500);
+  }, []);
+
   return (
     <DataContext>
       <Header />
