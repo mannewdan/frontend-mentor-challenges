f1be8ef792b3cd8b48818381cc149c06b5ef4f7a
mannewdan <109920968+mannewdan@users.noreply.github.com>
2023-04-11 13:28:22 -0500

    responsive sidebar

--- kanban-task-management-app/src/App.tsx
+++ kanban-task-management-app/src/App.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-
 import Header from "./components/Header";
 import Sidebar from "./components/Sidebar";
 import DataContext from "./context/DataContext";
--- kanban-task-management-app/src/components/Sidebar.tsx
+++ kanban-task-management-app/src/components/Sidebar.tsx
@@ -3,6 +3,8 @@ import hideIcon from "../assets/icon-hide-sidebar.svg";
 import showIcon from "../assets/icon-show-sidebar.svg";
 import lightIcon from "../assets/icon-light-theme.svg";
 import darkIcon from "../assets/icon-dark-theme.svg";
+import logoDark from "../assets/logo-light.svg";
+import logoLight from "../assets/logo-dark.svg";
 import Icon from "./Icon";
 import { useDataContext } from "../context/DataContext";
 
@@ -24,6 +26,8 @@ export default function Sidebar() {
 
   return (
     <nav className="sidebar">
+      <img className="logo" src={data.darkMode ? logoDark : logoLight}></img>
+
       <div className="scrollable-area">
         <h2 className="text-h-s">All Boards (3)</h2>
 
